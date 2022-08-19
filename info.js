1af2c2a2c0dc3e1bbae9a5eae761274821bbc017
TawanaState <88168991+TawanaState@users.noreply.github.com>
2022-08-19 19:57:56 +0200

    File Upload

---
+++ info.js
@@ -0,0 +1,24 @@
+window.myWebInfo = {
+    about1 : "My name is Tawananyasha Mukoriwo and I'm a passionate software developer, who loves to experiment with computers and getting to know more and more tools that help me create more robust, intelligent and user centric programs. ",
+    about2 : "I started programming when I was 14 years old and since then, I just can't stop coding! Its just so amazing how we are able to communicate with computers and getting them to do great stuff!",
+    subTitles : ["#Developer" , "#Freelancer", "#Designer"],
+    skillset : [
+        "///png.icons8.com/tensorflow/Color/2",
+        "///png.icons8.com/bootstrap/Color/2",
+        "///png.icons8.com/nodejs/Color/2",
+        "///png.icons8.com/typescript/Color/2",
+        "///png.icons8.com/django/Color/2",
+        "///png.icons8.com/xamarin/Color/2",
+        "https://cdn.rawgit.com/harthur-org/brain.js/ff595242/logo.svg",
+        "https://www.electronjs.org/assets/img/logo.svg"
+
+    ],
+    testimonials : [
+        {
+            title : "Asher Bvuwayi",
+            image : "/assets/img/empty_box_128px.png",
+            content : "I hereby testify, Tawana is freaking good!",
+            link : "wa.com"
+        }
+    ]
+};
